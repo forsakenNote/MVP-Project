3fe3c45cd0408bba836aeacf107038e7b1cdeb34
Sara Arruga <saraa@MacBook-Pro-de-Sara.local>
2022-07-23 09:59:16 +0200

    setting up

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import logo from './logo.svg';
+import Search from './components/Search';
+import Display from './components/Display';
 
 function App() {
+  const [location, setLocation] = useState("");
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Search placeholder="Enter a Location..." />
     </div>
   );
 }
---
+++ client/src/components/Search.js
@@ -0,0 +1,59 @@
+import React, { useState } from 'react';
+import './Search.css';
+
+const keyNoFilter = "dbdb379a-6076-41bc-a46c-fcbcb6578cb4";
+const baseNoFilter = "https://us-central1-mari-a5cc7.cloudfunctions.net/api";
+
+function Search({ placeholder, data }) {
+  const [loading, setLoading] = useState(false);
+  const [location, setLocation] = useState({""});
+  const [photos, setPhotos] = useState(null);
+  const [error, setError] = useState("");
+
+  const handleInputChange = (event) => {
+    setLocation(event.target.value)
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("form button clicked");
+    
+    setLoading(true);
+    setError("");
+
+    let photos = await getPhotos();
+    setPhotos(photos);
+
+    setLocation("");
+    setLoading(false);
+  }
+
+  const getPhotos = async () => {
+    try {
+      let response = await fetch(url);
+      if (response.ok) {
+        let data = await response.json();
+        return data;
+      } else {
+        setError(`Server error: ${response.status} ${response.statusText}`); //on the server, but data does not exist or wrong parameters.
+      }
+    } catch (err) {
+      setError("Network error:", err.message); //error 500 not in the server
+    }
+
+    return null;
+  }
+
+  return (
+    <div className="search">
+      <div className="searchInputs">
+        <input type="text" placeholder={placeholder}/>
+      </div>
+      <div className="dataResults"></div>
+
+      <button className="submitButton" onClick = {handleSubmit}>Submit</button>
+    </div>
+  )
+}
+
+export default Search;
