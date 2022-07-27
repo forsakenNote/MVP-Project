834d3121e28e14707dc29bb0337d3c9e8dd7f78a
Sara Arruga <saraa@MacBook-Pro-de-Sara.local>
2022-07-27 11:00:35 +0200

    changes

--- client/src/components/MapApi.js
+++ client/src/components/MapApi.js
@@ -1,11 +1,12 @@
-import {MapContainer, TileLayer, Marker, Popup} from 'react-leaflet';
+import "leaflet/dist/leaflet.css";
 import './map.css'
+import {MapContainer, TileLayer, Marker, Popup} from 'react-leaflet';
 
 
 export default function MapApi () {
     return (
-        <div>
-            <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
+        <div className="App">
+            <MapContainer className="map" center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
             <TileLayer
                 attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                 url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
